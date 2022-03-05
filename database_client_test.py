407d4455f1f09ee048efa0306d734bde2e7c6a86
Michael Metzler <michael.r.metzler@gmail.com>
2022-03-05 14:40:49 -0500

    database testing

--- database_client_test.py
+++ database_client_test.py
@@ -4,12 +4,11 @@ import pandas as pd
 con = sqlite3.connect("covid.sqlite")
 cur = con.cursor()
 
-cur.execute('SELECT * FROM covid')
-data = cur.fetchall()
-print(data[-1])
+covid_df = pd.read_sql_query('SELECT * FROM cases WHERE state_name = "Texas" ORDER BY date DESC', con)
+print("MD covid cases test:")
+print(covid_df.sort_values(by=['cases_per_100k_7_day_count']))
 
-
-covid_df = pd.read_sql_query('SELECT * FROM covid WHERE state_name = "Maryland"', con)
-print(max(covid_df.index))
-print(covid_df.head(10))
+vax_df = pd.read_sql_query('SELECT * FROM vaccinations WHERE recip_state = "MD"', con)
+print("MD covid vaccinations test:")
+print(vax_df.head(10))
 con.close()
